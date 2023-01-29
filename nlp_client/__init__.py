b883878bf5659a2760642b78e8828e1af0f200c1
Game Limsila <limsila.limsila@yahoo.com>
2023-01-29 18:31:21 +0700

    fix broken build

--- nlp_client/__init__.py
+++ nlp_client/__init__.py
@@ -1,10 +1,10 @@
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 """ 
 Date: 28 Jan 2023
 """
-
 import requests
 from ratfin import *
+import json
 
 def speak(text: str) :
     try :
@@ -21,7 +21,7 @@ def speak(text: str) :
 def ww_listen(): # go to Wakeword server
     try:
         response = requests.get("http://localhost:5100/").json() # wakeword get
-        while response["confidence"] < 0.6:
+        while response["confidence"] < 0.62:
             speak("Sorry I didn't get that, could you speak louder or rephrase the sentence?")
             response = requests.get("http://localhost:5101/").json() # asr get
         return response
@@ -31,36 +31,77 @@ def ww_listen(): # go to Wakeword server
 def listen(return_json=False): # go to ASR server, By-pass wakeword
     try:
         response = requests.get("http://localhost:5101/").json() # asr get
-        while response["confidence"] < 0.6:
+        while response["confidence"] < 0.62:
             speak("Sorry I didn't get that, could you speak louder or rephrase the sentence?")
             response = requests.get("http://localhost:5101/").json() # asr get
         return response
     except Exception as e:
         printclr(e,"red")
 
-# import time
-# import json 
-# start = time.time()
-# time.sleep(1)
+def get_intent(predicted_text):
+        response = {
+            "recipient_id": "bot",
+            "body": predicted_text
+        }
 
-# print(time.time() - start)
+        #TODO try and except UGLY.......
+        #* Get intent
+        r = requests.post(url= "http://localhost:5005/webhooks/rest/webhook", 
+                    json={"sender": "bot", "message": predicted_text}
+                    )
+        # printclr(r.json(),"cyan")
+        if r.json() == []: #TODO FIX THIS BULLSHIT
+            print("Low confidence level")
+            response.update({"confidence": 0})
+        else:
+            rasa_json = r.json()[0]['text']
+            rasa_json = json.loads(rasa_json)
+            # printclr(rasa_json,"red")
+            # printclr(response,"red")
+            response.update(rasa_json)
+            # printclr(json.dumps(response, indent=4),"blue")
+
+            #* Get confidence
+            r = requests.post(url= "http://localhost:5005/model/parse", 
+                        json={"text": predicted_text}
+                        )
+            if r.json() == []:
+                print("Low confidence level")
+                response.update({"confidence": 0})
+            else:
+                confidence = r.json()['intent_ranking'][0]['confidence']
+                indent_name = r.json()['intent_ranking'][0]['name']
+                printclr(f"\t{confidence=}","blue")
+                printclr(f"\t{indent_name=}","blue")
+                # printclr(f"\t{json.dumps(response, indent=4)}","blue")
+
+                
+                # printclr(dict(json.dumps(r.json()[0]['text'])),"red")
+                if indent_name == str(response["intent"]):
+                    response.update({"confidence": confidence})
+                else:
+                    printclr("its not the same","red")
+        # print(response)
+        printclr(f"\t{json.dumps(response, indent=4)}","blue")
+        printclr(f"\tlisten() sending back...","green")
+        return response
+
+
 def main():
-    #* for building package comment all the main otherwise it will run the main everytime you import the package
-    # clearterm() 
+    clearterm() 
 
     # print("this is speak")
     # print(speak("say stop motherfucker"))
     # print(speak("say "))
-    # # print(listen())
-    # # while True:
-    #     # x = dict(ww_listen())
-    # # print(ww_listen())
+    # print(listen())
+    # while True:
+        # x = dict(ww_listen())
+    # print(ww_listen())
     # print(json.dumps(listen(), indent=4))
     # print(json.dumps(ww_listen(), indent=4))
         # if x['intent'] == "stop":
         #     printclr("STOPPINGGGG........","red")
         #     speak("stop")
-    pass
 
 main()
     # print(ww_listen())
